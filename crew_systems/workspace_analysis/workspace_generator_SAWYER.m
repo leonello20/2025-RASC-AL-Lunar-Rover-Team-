1ac4a73ecad17b78e5c434e260bceef6bb728b44
Romeo-Perl-14 <romeoperlstein14@gmail.com>
2025-02-01 15:15:56 -0500

    balling?

---
+++ crew_systems/workspace_analysis/workspace_generator_SAWYER.m
@@ -0,0 +1,10 @@
+
+% RECALL THAT DH TABLES START i=0 IS THE BASE LINK, i.e [0,0,0]
+% units are degrees and meters
+
+% Make a DH table for refernce
+% __|       d(i)        theta(i)       a(i-1)      alpha(i-1)
+% 1 |       0.25          th1             0           0
+% 2 |       0             th2             0           90
+% 3 |       0             th3             0.5         0
+% 4 |       0.4           th4             0          -90   <- EE  
